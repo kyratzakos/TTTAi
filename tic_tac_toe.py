91bada74e904a906dcfd2596b5046f2dc2610223
kyratzakos <109310356+kyratzakos@users.noreply.github.com>
2023-08-28 18:24:31 +0300

    Update tic_tac_toe.py
    - Added function that returns True or False, if the game is over
    - Added function that evaluates the board based on who's winning
    -  Added the minimax function that utilizes the above function and returns the best possible move for the next player

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 def print_board(board):
   #Prints the given board
   print("|-------------|")
@@ -45,3 +47,48 @@ def has_won(board, player):
   if board[0][2] == player and board[1][1] == player and board[2][0] == player:
     return True
   return False
+
+def game_is_over(board):
+  return has_won(board, "X") or has_won(board, "O") or len(available_moves(board)) == 0
+
+def evaluate_board(board):
+  # Evaluating the leaves of the game tree
+  if has_won(board, "X"):
+    return 1
+  elif has_won(board, "O"):
+    return -1
+  else:
+    return 0
+
+def minimax(input_board, is_maximizing):
+  if game_is_over(input_board):
+    # Base case : If the game is over, the value of the board is returned
+    return [evaluate_board(input_board), ""]
+  if is_maximizing:
+    # The maximizing player : The best value starts at the lowest possible value
+    best_value = -float("Inf")
+    best_move = ""
+    for move in available_moves(input_board):
+      # Loop through all the available moves
+      new_board = deepcopy(input_board)
+      select_space(new_board, move, "X")
+      # Recursively finding the opponent's best move
+      hypothetical_value = minimax(new_board, False)[0]
+      if hypothetical_value > best_value:
+        # Update best value if you found a better hypothetical value
+        best_value = hypothetical_value
+        best_move = move
+    return [best_value, best_move]
+  else:
+    # The minimizing player : The best value starts at the highest possible value
+    best_value = float("Inf")
+    best_move = ""
+    for move in available_moves(input_board):
+      new_board = deepcopy(input_board)
+      select_space(new_board, move, "O")
+      hypothetical_value = minimax(new_board, True)[0]
+      if hypothetical_value < best_value:
+        best_value = hypothetical_value
+        best_move = move
+    return [best_value, best_move]
+    
