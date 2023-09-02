2c15510359f3c10a1fc781f5d906349b8507af1c
Apostolos Kyratzakos <kyratzakos.ap@gmail.com>
2023-09-02 11:41:42 +0300

    Edited gamev2.py -Optimized the code -Added boilerplate protection

--- gamev2.py
+++ gamev2.py
@@ -6,160 +6,71 @@ from tic_tac_toe import *
 
 def nextstep(board):
     # This function will be called each time after the user's turn
+    global buttons
+    if game_is_over(board):
+        # This part will disable the buttons, when the game is over
+        for button in buttons:
+            button.configure(state="disabled", bg="#bcbcbc")
+
     global turn
-    if turn == "AI":
+    if turn == "AI" and not game_is_over(board):
         turn = "Player"
         move = minimax(board, True)[1]
         select_space(board, move, "O")
-        # The following part needs optimizing
-        if move == 1:
-            update1("O", board, move)
-        elif move == 2:
-            update2("O", board, move)
-        elif move == 3:
-            update3("O", board, move)
-        elif move == 4:
-            update4("O", board, move)
-        elif move == 5:
-            update5("O", board, move)
-        elif move == 6:
-            update6("O", board, move)
-        elif move == 7:
-            update7("O", board, move)
-        elif move == 8:
-            update8("O", board, move)
-        elif move == 9:
-            update9("O", board, move)
+        update("O", board, move, buttons[move-1], expression_fields[move-1])
     else:
         turn = "AI"
+    print_board(board)
 
-    if game_is_over(board):
-        # This part will disable the buttons, when the game is over
-        one_button.configure(state="disabled", bg="#bcbcbc")
-        two_button.configure(state="disabled", bg="#bcbcbc")
-        three_button.configure(state="disabled", bg="#bcbcbc")
-        four_button.configure(state="disabled", bg="#bcbcbc")
-        five_button.configure(state="disabled", bg="#bcbcbc")
-        six_button.configure(state="disabled", bg="#bcbcbc")
-        seven_button.configure(state="disabled", bg="#bcbcbc")
-        eight_button.configure(state="disabled", bg="#bcbcbc")
-        nine_button.configure(state="disabled", bg="#bcbcbc")
-
-
-# The functions updateX will update the text variable of each button, whether the user or the AI presses it
-def update1(symbol, board, move):
-    expression_field1.set(symbol)
+def update(symbol, board, move, button, field):
+    # This function will update the text variable of each button, whether the user or the AI presses it
+    field.set(symbol)
     select_space(board, move, symbol)
+    button.configure(state="disabled")
     nextstep(board)
-    one_button.configure(state="disabled")
 
+def create_button(row, column, expression_field_index):
+    # This function will create 9 buttons inside a 3x3 grid
+    button = Button(
+        game_frame,
+        textvariable=expression_fields[expression_field_index],
+        fg="black",
+        width=12,
+        height=6,
+        bd=0,
+        bg="#eee",
+        cursor="hand2",
+        command=lambda idx=expression_field_index: update(symbol, my_board, idx + 1, buttons[idx], expression_fields[idx])
+    )
+    button.grid(row=row, column=column, padx=1, pady=1)
+    return button
 
-def update2(symbol, board, move):
-    expression_field2.set(symbol)
-    select_space(board, move, symbol)
-    nextstep(board)
-    two_button.configure(state="disabled")
+if __name__ == '__main__':
+    # Window settings and frame settings
+    customtkinter.set_appearance_mode("dark")
+    root = customtkinter.CTk()
+    root.geometry('800x600')
+    root.resizable(False, False)
+    root.title("Tic Tac Toe Game v2")
+    root.iconbitmap(default="icon2.ico")
 
+    menu_frame = Frame(root, width=200, height=600, bd=0, highlightbackground="black", bg="#2c69a5", highlightcolor="black",
+                       highlightthickness=1)
+    menu_frame.pack(side=LEFT)
+    game_frame = Frame(root, width=500, height=500, bg="grey")
+    game_frame.pack(expand=True)
 
-def update3(symbol, board, move):
-    expression_field3.set(symbol)
-    select_space(board, move, symbol)
-    nextstep(board)
-    three_button.configure(state="disabled")
+    expression_fields = [StringVar() for _ in range(9)]
+    symbol = "X"
+    turn = "AI"
+    my_board = [
+        ["1", "2", "3"],
+        ["4", "5", "6"],
+        ["7", "8", "9"]
+    ]
+    buttons = []
+    for i in range(9):
+        button = create_button(i // 3, i % 3, i)
+        buttons.append(button)
 
-
-def update4(symbol, board, move):
-    expression_field4.set(symbol)
-    select_space(board, move, symbol)
-    nextstep(board)
-    four_button.configure(state="disabled")
-
-
-def update5(symbol, board, move):
-    expression_field5.set(symbol)
-    select_space(board, move, symbol)
-    nextstep(board)
-    five_button.configure(state="disabled")
-
-
-def update6(symbol, board, move):
-    expression_field6.set(symbol)
-    select_space(board, move, symbol)
-    nextstep(board)
-    six_button.configure(state="disabled")
-
-
-def update7(symbol, board, move):
-    expression_field7.set(symbol)
-    select_space(board, move, symbol)
-    nextstep(board)
-    seven_button.configure(state="disabled")
-
-
-def update8(symbol, board, move):
-    expression_field8.set(symbol)
-    select_space(board, move, symbol)
-    nextstep(board)
-    eight_button.configure(state="disabled")
-
-
-def update9(symbol, board, move):
-    expression_field9.set(symbol)
-    select_space(board, move, symbol)
-    nextstep(board)
-    nine_button.configure(state="disabled")
-
-
-my_board = [
-    ["1", "2", "3"],
-    ["4", "5", "6"],
-    ["7", "8", "9"]
-]
-
-# Window settings and frame settings
-customtkinter.set_appearance_mode("dark")
-root = customtkinter.CTk()
-root.geometry('800x600')
-root.resizable(False, False)
-root.title("Tic Tac Toe Game v2")
-root.iconbitmap(default="icon2.ico")
-
-menu_frame = Frame(root, width=200, height=600, bd=0, highlightbackground="black", bg="#2c69a5", highlightcolor="black",
-                   highlightthickness=1)
-menu_frame.pack(side=LEFT)
-
-game_frame = Frame(root, width=500, height=500, bg="grey")
-game_frame.pack(expand=True)
-
-expression_field1 = StringVar()
-expression_field2 = StringVar()
-expression_field3 = StringVar()
-expression_field4 = StringVar()
-expression_field5 = StringVar()
-expression_field6 = StringVar()
-expression_field7 = StringVar()
-expression_field8 = StringVar()
-expression_field9 = StringVar()
-symbol = "X"
-turn = "AI"
-
-one_button = Button(game_frame, textvariable=expression_field1, fg="black", width=12, height=6, bd=0, bg="#eee", cursor="hand2", command=lambda: update1(symbol, my_board, 1))
-one_button.grid(row=0, column=0, padx=1, pady=1)
-two_button = Button(game_frame, textvariable=expression_field2, fg="black", width=12, height=6, bd=0, bg="#eee", cursor="hand2", command=lambda: update2(symbol, my_board, 2))
-two_button.grid(row=0, column=1, padx=1, pady=1)
-three_button = Button(game_frame, textvariable=expression_field3, fg="black", width=12, height=6, bd=0, bg="#eee", cursor="hand2", command=lambda: update3(symbol, my_board, 3))
-three_button.grid(row=0, column=2, padx=1, pady=1)
-four_button = Button(game_frame, textvariable=expression_field4, fg="black", width=12, height=6, bd=0, bg="#eee", cursor="hand2", command=lambda: update4(symbol, my_board, 4))
-four_button.grid(row=1, column=0, padx=1, pady=1)
-five_button = Button(game_frame, textvariable=expression_field5, fg="black", width=12, height=6, bd=0, bg="#eee", cursor="hand2", command=lambda: update5(symbol, my_board, 5))
-five_button.grid(row=1, column=1, padx=1, pady=1)
-six_button = Button(game_frame, textvariable=expression_field6, fg="black", width=12, height=6, bd=0, bg="#eee", cursor="hand2", command=lambda: update6(symbol, my_board, 6))
-six_button.grid(row=1, column=2, padx=1, pady=1)
-seven_button = Button(game_frame, textvariable=expression_field7, fg="black", width=12, height=6, bd=0, bg="#eee", cursor="hand2", command=lambda: update7(symbol, my_board, 7))
-seven_button.grid(row=2, column=0, padx=1, pady=1)
-eight_button = Button(game_frame, textvariable=expression_field8, fg="black", width=12, height=6, bd=0, bg="#eee", cursor="hand2", command=lambda: update8(symbol, my_board, 8))
-eight_button.grid(row=2, column=1, padx=1, pady=1)
-nine_button = Button(game_frame, textvariable=expression_field9, fg="black", width=12, height=6, bd=0, bg="#eee", cursor="hand2", command=lambda: update9(symbol, my_board, 9))
-nine_button.grid(row=2, column=2, padx=1, pady=1)
-
-root.mainloop()
+    root.mainloop()
